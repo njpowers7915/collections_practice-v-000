156e487c3a9e40d26539a1bf459ff70fb0d3059e
Nicholas Powers <njpowers7915@gmail.com>
2018-05-07 14:12:54 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,35 @@
+def sort_array_asc(array)
+  
+end
 
+def sort_array_desc(array)
+
+end
+
+def sort_array_char_count(array)
+  
+end
+
+def swap_elements(array)
+  
+end
+
+def reverse_array(array)
+  
+end
+
+def kesha_maker(string)
+  
+end
+
+def find_a(array)
+  
+end
+
+def sum_array(array)
+  
+end
+
+def add_s(array)
+  
+end
